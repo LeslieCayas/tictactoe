11275a97fc030d76faebc54b62a756c8ff42989b
lesliec <leslie.cayas@gmail.com>
2021-05-03 22:43:29 +1000

    able to alternate between X and O

--- main.js
+++ main.js
@@ -1,13 +1,112 @@
-// P1 = red
-// P2 = green
-// store board into an array 
-//-- let board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-//-- use logic to determine placement/winner 
 
-/* function displayTheBoard(board) { 
-  //create divs for us and fill them with correct values 
+// TEST ZONE: area selected is cell 1
+/*let box = document.querySelector('.a.x')
+box.addEventListener('click', function () {
+  if (box.innerHTML === 'O' || box.innerHTML === 'X') {
+    alert('This spot is taken!')
+  } else {
+    console.log('clicked')
+  }
+})*/
+
+/* Creating player tokens */
+
+
+let boxes = document.querySelectorAll('div')
+
+for (i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener('click', function(event) {
+    alternatePlayer(event)
+    console.log(event)
+  })
+  }
+
+
+function placeX(event) {
+  event.target.innerText = 'X'
 }
-*/
-/*
 
-*/
+function placeO(event) {
+  event.target.innerText = 'O'
+}
+
+let playerOneStatus = true
+function alternatePlayer(event) {
+  console.log(playerOneStatus)
+  if (playerOneStatus) {
+    placeX(event) //puts an X
+    playerOneStatus = false
+  } else if (playerOneStatus === false) {
+    placeO(event) //puts an O
+    playerOneStatus = true
+  }
+}
+/* Alternate between players */
+// the if statement for a taken position works
+
+
+/* Three in a ROW */
+const rowOne = Array.from(document.querySelectorAll('.a'))
+const rowTwo = Array.from(document.querySelectorAll('.b'))
+const rowThree = Array.from(document.querySelectorAll('.c'))
+let rows = [rowOne, rowTwo, rowThree]
+console.log(rowOne)
+console.log(rowTwo)
+console.log(rows[0][1])
+//turns all cells green after clicking three in the second row regardless of character
+
+for (i = 0; i < rowTwo.length; i++) {
+  rowTwo[i].addEventListener('click', function (event) {
+    //event.target.innerText = 'X'
+    let rowCheck = true
+
+    for (let i = 0; i < rowTwo.length; i++) {
+      if (rowTwo[i].innerText !== 'X' && rowTwo[i].innerText !== 'O') {
+        rowCheck = false
+      }
+    }
+    for (let i = 0; i < rowTwo.length; i++) {
+      if (rowCheck) {
+        rowTwo[i].style.backgroundColor = 'green'
+      }
+    }
+  })
+}
+
+// adds click listener to all boxes
+// can probably be done in another way
+/*for (i = 0; i < rows.length; i++) {
+  for (j=0; j < rows[i].length; j++) {
+    rows[i][j].addEventListener('click', function(event) {
+      console.log('clicked')
+      event.target.innerText = 'X'
+      let rowCheck = true
+      
+    })
+  }
+}*/
+/* for each row and for each item in each row, add an event listener.
+when the element is clicked,*/
+
+function getRowValues(rowElements) {
+  let rowArray = []
+  for ( let i = 0; i < rowElements.length; i++) {
+    rowArray.push(rowElements[i].innerText)
+  }
+  console.log(rowArray)
+}
+let row1Values = getRowValues(document.querySelectorAll('.a'))
+let row2Values = getRowValues(document.querySelectorAll('.b'))
+let row3Values = getRowValues(document.querySelectorAll('.c'))
+
+function getColumnValues(columnElements) {
+  let columnArray = []
+  for ( let i = 0; i < columnElements.length; i++ ) {
+    columnArray.push(columnElements[i].textContent)
+  }
+  console.log(columnArray)
+}
+let column1Values = getColumnValues(document.querySelectorAll('.x'))
+let column2Values = getColumnValues(document.querySelectorAll('.y'))
+let column3Values = getColumnValues(document.querySelectorAll('.z'))
+
--- planning.md
+++ planning.md
@@ -34,14 +34,18 @@ X | _ | _ | _ |
 Y | _ | _ | _ |
 Z | _ | _ | _ |
 
-<!-- PLAYERS -->
-assign player 1 = 1
-when clicked, change innertext to 1
-if text has 1 or 2, cannot be clicked again
+change class with cell # and store winning combinations
 
-assign player 2 = 2
-when clicked, change innertext to 1
-if text has 1 or 2, cannot be clicked again
+<!-- PLAYERS -->
+assign player 1 = X
+assign player 2 = O
+set player1 status as true
+if it is player 1's turn, place an X in the clicked position
+--when clicked, change innertext to X and change player1 status to false
+--if text has 1 or 2, cannot be clicked again
+if it is not player 1's turn, it is player 2's turn so place an O in the clicked position
+--when clicked, change innertext to O and change player1 status to true
+--if text has 1 or 2, cannot be clicked again
 
 <!-- GAME FUNCTIONALITY -->
 functions to check if there are 3 in one of the rows, columns, diagonals
@@ -58,9 +62,26 @@ The above is the same but looping through the columns (xyz)
 FOR DIAGONALS
 would need to have the same text in either:
 -diagonal type 1(a[0]z[0], b[1]y[1], c[2]z[2]) 
+----AKA (.a.z and .b.y and  .c.z)
 -diagonal type 2 (z[0]a[2], b[1]y[1], x[2]c[0])
+----AKA (.a,.z and .b.y and .c.x)
+
 create a variable for each diagonal
 for each box in the diagonal:
 --assume that there are 3 X/O in either of the diagonals
 --if there is one odd box, state that there isn't 3 X/O in a diagonal
 --alternatively, if there are 3 in diagonal 1 or diagonal 2, then the player who placed 3 in a diagonal, wins
+
+/// getting row values
+grab the textContent of a selected row
+
+// for each item in the selected row
+add an event listener that will: 
+-- assume text content is the same in all cells of the row = xxx or ooo
+-- for each item in the selected row
+---- if one of the cells are not the same as the first cell
+------ state that the row is does not have the same text in each item
+-- for each item in the selected row
+---- if all the items have the same content
+------ turn the background to green
+-- alert winner 
