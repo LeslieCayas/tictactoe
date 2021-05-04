73d25a43cb6ebf89537a98f3c848f1164b960cbc
lesliec <leslie.cayas@gmail.com>
2021-05-04 21:57:48 +1000

    can alternate player and prevent player from clicking a taken spot

--- main.js
+++ main.js
@@ -13,15 +13,20 @@ box.addEventListener('click', function () {
 
 
 let boxes = document.querySelectorAll('div')
+let playerOneStatus = true
 
 for (i = 0; i < boxes.length; i++) {
-  boxes[i].addEventListener('click', function(event) {
+  boxes[i].addEventListener('click', function (event) {
     alternatePlayer(event)
-    console.log(event)
+    let row1Values = getRowValues(document.querySelectorAll('.a'))
+    let row2Values = getRowValues(document.querySelectorAll('.b'))
+    let row3Values = getRowValues(document.querySelectorAll('.c'))
+    let column1Values = getColumnValues(document.querySelectorAll('.x'))
+    let column2Values = getColumnValues(document.querySelectorAll('.y'))
+    let column3Values = getColumnValues(document.querySelectorAll('.z'))
+
   })
-  }
-
-
+}
 function placeX(event) {
   event.target.innerText = 'X'
 }
@@ -30,15 +35,22 @@ function placeO(event) {
   event.target.innerText = 'O'
 }
 
-let playerOneStatus = true
 function alternatePlayer(event) {
   console.log(playerOneStatus)
   if (playerOneStatus) {
-    placeX(event) //puts an X
-    playerOneStatus = false
+    if (event.target.innerText == 'X' || event.target.innerText == 'O') {
+      alert('something here')
+    } else {
+      placeX(event) //puts an X
+      playerOneStatus = false
+    }
   } else if (playerOneStatus === false) {
-    placeO(event) //puts an O
-    playerOneStatus = true
+    if (event.target.innerText == 'X' || event.target.innerText == 'O') {
+      alert('something here')
+    } else {
+      placeO(event) //puts an O
+      playerOneStatus = true
+    }
   }
 }
 /* Alternate between players */
@@ -46,16 +58,77 @@ function alternatePlayer(event) {
 
 
 /* Three in a ROW */
-const rowOne = Array.from(document.querySelectorAll('.a'))
-const rowTwo = Array.from(document.querySelectorAll('.b'))
-const rowThree = Array.from(document.querySelectorAll('.c'))
+const rowOne = document.querySelectorAll('.a')
+const rowTwo = document.querySelectorAll('.b')
+const rowThree = document.querySelectorAll('.c')
 let rows = [rowOne, rowTwo, rowThree]
 console.log(rowOne)
 console.log(rowTwo)
-console.log(rows[0][1])
+//console.log(rows[0][1].textContent)
+
+/* for each row and for each item in each row, add an event listener.
+when the element is clicked,*/
+
+// Get Row Elements
+function getRowValues(rowElements) {
+  let rowArray = []
+  for (let i = 0; i < rowElements.length; i++) {
+    rowArray.push(rowElements[i].innerText)
+  }
+  return rowArray // gives the array back to be used later
+}
+let row1Values = getRowValues(document.querySelectorAll('.a'))
+let row2Values = getRowValues(document.querySelectorAll('.b'))
+let row3Values = getRowValues(document.querySelectorAll('.c'))
+
+
+// Get Column Elements
+function getColumnValues(columnElements) {
+  let columnArray = []
+  for (let i = 0; i < columnElements.length; i++) {
+    columnArray.push(columnElements[i].textContent)
+  }
+  return columnArray // gives the array back to be used later
+}
+let column1Values = getColumnValues(document.querySelectorAll('.x'))
+let column2Values = getColumnValues(document.querySelectorAll('.y'))
+let column3Values = getColumnValues(document.querySelectorAll('.z'))
+
+// Get Diagonal Elements
+
+console.log(row3Values)
+console.log(row2Values)
+console.log(row1Values)
+
+
+/*
+analyse the row
+assume the row has the same content in each cell
+check through each cell
+if the cell has diff content state that the row is uneven
+check through each cell again
+if the rows are all the same declare that the row is found
+*/
+function rowCheck(row) {
+  for (let i = 0; i < row.length; i++) {
+    let threeInARow = true
+    for (let i = 0; i < row.length; i++) {
+      if (row[i].innerText !== 'X' || row[i].innerText !== 'O') {
+        threeInARow = false
+      }
+    }
+    for (let i = 0; i < row.length; i++) {
+      if (threeInARow) {
+        row[i].style.backgroundColor = 'green'
+      }
+    }
+  }
+}
+rowCheck(row1Values)
+
 //turns all cells green after clicking three in the second row regardless of character
 
-for (i = 0; i < rowTwo.length; i++) {
+/*for (i = 0; i < rowTwo.length; i++) {
   rowTwo[i].addEventListener('click', function (event) {
     //event.target.innerText = 'X'
     let rowCheck = true
@@ -71,7 +144,7 @@ for (i = 0; i < rowTwo.length; i++) {
       }
     }
   })
-}
+}*/
 
 // adds click listener to all boxes
 // can probably be done in another way
@@ -81,32 +154,8 @@ for (i = 0; i < rowTwo.length; i++) {
       console.log('clicked')
       event.target.innerText = 'X'
       let rowCheck = true
-      
+
     })
   }
 }*/
-/* for each row and for each item in each row, add an event listener.
-when the element is clicked,*/
-
-function getRowValues(rowElements) {
-  let rowArray = []
-  for ( let i = 0; i < rowElements.length; i++) {
-    rowArray.push(rowElements[i].innerText)
-  }
-  console.log(rowArray)
-}
-let row1Values = getRowValues(document.querySelectorAll('.a'))
-let row2Values = getRowValues(document.querySelectorAll('.b'))
-let row3Values = getRowValues(document.querySelectorAll('.c'))
-
-function getColumnValues(columnElements) {
-  let columnArray = []
-  for ( let i = 0; i < columnElements.length; i++ ) {
-    columnArray.push(columnElements[i].textContent)
-  }
-  console.log(columnArray)
-}
-let column1Values = getColumnValues(document.querySelectorAll('.x'))
-let column2Values = getColumnValues(document.querySelectorAll('.y'))
-let column3Values = getColumnValues(document.querySelectorAll('.z'))
 
