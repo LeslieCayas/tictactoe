122fc51886b6ccff4418a38dce8b77cf154fc029
lesliec <leslie.cayas@gmail.com>
2021-05-08 03:20:30 +1000

    Revert "game functions complete, css tokens complete"
    This reverts commit 2634f6929143ce7422cc8be1edf0f473d1721be5.

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
 # My tic-tac-toe game
-
-This is my tic-tac-toe game using "vanilla Javascript", HTML and CSS.
-To develop this game, a grid was made using HTML and CSS where the rows and columns were assigned by giving classes.
-
-APPROACH:
-1. Initially wrote out functions to check row,column and diagonal
-2. Further condensed functions as they were found to be repetitive
-3. After condensing the functions, each function was given a separate name and placed in an even listener to allow general readability
-
-FUTURE IMPROVEMENTS:
-- Some repetition is still evident in the current code i.e. threeInARow and threeInAColumn function can be further condensed
-- Instead of
--- main.js
+++ main.js
@@ -1,65 +1,86 @@
-// Initializing core variables i.e. the grid cells, the 3 rows, the 3 columns, the 2 diagonals
-const boxes = document.querySelectorAll("div")
-const gameStatus = document.querySelector("h2")
+let boxes = document.querySelectorAll("div")
 let playerOneStatus = true
 let row1Values, row2Values, row3Values, column1Values, column2Values, column3Values, diagonalsValues
-let movesCounter = 0
-
-// Overarching event handler for the game
-// Adds an event listener to each grid cell
+let counter = 0
 for (i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener("click", gamePlay)
+    boxes[i].addEventListener("click", function(event) {
+        alternatePlayer(event)
+        row1Values = getRowValues(document.querySelectorAll(".a"))
+        row2Values = getRowValues(document.querySelectorAll(".b"))
+        row3Values = getRowValues(document.querySelectorAll(".c"))
+        column1Values = getColumnValues(document.querySelectorAll(".x"))
+        column2Values = getColumnValues(document.querySelectorAll(".y"))
+        column3Values = getColumnValues(document.querySelectorAll(".z"))
+        diagonalsValues = getDiagonalValues(document.querySelectorAll('div'))
+        threeInADiagonal(diagonalsValues[0])
+        threeInADiagonal(diagonalsValues[1])
+        threeInARow(row1Values)
+        threeInARow(row2Values)
+        threeInARow(row3Values)
+        threeInAColumn(column1Values)
+        threeInAColumn(column2Values)
+        threeInAColumn(column3Values)
+        counter++
+        if (counter === 9) {
+            alert("draw!")
+        }
+    })
 }
 
-// Get the values of each row, column and diagonal then uses the values to 
-// determine if there are 3 X's or O's in a row, column or diagonal, or if neither player as won
-function gamePlay(event) {
+// Get Row Elements and Three in a Row
+function getRowValues(rowElements) {
+    let rowArray = []
 
-    // Alternate between player 1 and player 2
-    alternatePlayer(event)
+    for (let i = 0; i < rowElements.length; i++) {
+        rowArray.push(rowElements[i].innerText)
+    }
 
-    // Get values of each row, column and diagonal
-    row1Values = lineValues(document.querySelectorAll(".r1"))
-    row2Values = lineValues(document.querySelectorAll(".r2"))
-    row3Values = lineValues(document.querySelectorAll(".r3"))
-    column1Values = lineValues(document.querySelectorAll(".c1"))
-    column2Values = lineValues(document.querySelectorAll(".c2"))
-    column3Values = lineValues(document.querySelectorAll(".c3"))
-    diagonalsValues = getDiagonalValues(document.querySelectorAll('div'))
+    return rowArray // gives the array back to be used later
 
-    // Determine if there are 3 of the same token in a row, column or diagonal
-    lineOfThree(diagonalsValues[0])
-    lineOfThree(diagonalsValues[1])
-    lineOfThree(row1Values)
-    lineOfThree(row2Values)
-    lineOfThree(row3Values)
-    lineOfThree(column1Values)
-    lineOfThree(column2Values)
-    lineOfThree(column3Values)
+}
+function threeInARow(array) {
+    let rowCheck = true
 
-    // If the max number of moves (9) is reached, it is considered a draw
-    movesCounter++
-    if (movesCounter === 9) {
-        alert("draw!")
-        endGame()
+    for (let i = 0; i < array.length; i++) {
+        if (array[0] !== array[i] || array[i] === '') {
+            rowCheck = false
+        }
+    }
+
+    if (rowCheck) {
+        alert("There is 3 in a row")
+    }
+
+}
+
+
+// Get Column Elements and Three in a Column
+function getColumnValues(columnElements) {
+    let columnArray = []
+
+    for (let i = 0; i < columnElements.length; i++) {
+        columnArray.push(columnElements[i].textContent)
+    }
+
+    return columnArray // gives the array back to be used later
+}
+
+function threeInAColumn(array) {
+    let columnCheck = true
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[0] !== array[i] || array[i] === '') {
+            columnCheck = false
+            break
+        } 
+    }
+    if (columnCheck) {
+        alert("There is 3 in a column")
     }
 }
 
 
-/* Get Row/Column Elements and Determines if there are Three in a Row */
-// Gets the values of each row and column of the board
-function lineValues(rowOrColumnElements) {
-    let lineArray = []
-
-    for (let i = 0; i < rowOrColumnElements.length; i++) {
-        lineArray.push(rowOrColumnElements[i].innerText)
-    }
-
-    return lineArray // gives the values of each Row and Column as an array
-}
-
-/* Gets Diagonal Elements and stores them as 2 arrays in an array */
-// Groups 2 diagonals based on the row and column position, these diagonals are grouped into a Diagonals variable
+// Get Diagonal Elements and Three in a Diagonal
 function getDiagonalValues(div) {
     let diagonalArray = []
     let diagonalOneArray = []
@@ -67,47 +88,40 @@ function getDiagonalValues(div) {
 
     for (let i = 0; i < div.length; i++) {
 
-        if (div[i].className === "r1 c1" || div[i].className === "r2 c2" || div[i].className === "r3 c3") {
+        if (div[i].className === "a x" || div[i].className === "b y" || div[i].className === "c z") {
             diagonalOneArray.push(div[i].innerText)
 
-        } if (div[i].className === "r1 c3" || div[i].className === "r2 c2" || div[i].className === "r3 c1") {
+        } if (div[i].className === "a z" || div[i].className === "b y" || div[i].className === "c x") {
             diagonalTwoArray.push(div[i].innerText)
         }
     }
-
     diagonalArray.push(diagonalOneArray)
     diagonalArray.push(diagonalTwoArray)
     return diagonalArray
 }
-
-// Checks if there are three of the same tokens in each Row, Column or Diagonal
-function lineOfThree(elementsArray) {
-    let rowCheck = true
-
-    for (let i = 0; i < elementsArray.length; i++) {
-        if (elementsArray[0] !== elementsArray[i] || elementsArray[i] === '') {
-            rowCheck = false
+function threeInADiagonal(array) {
+    let diagonalCheck = true
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] != array[0] || array[i] === '') {
+            diagonalCheck = false
         }
     }
-
-    if (rowCheck) {
-        if (playerOneStatus !== false) {
-            alert("player two wins!")
-            gameStatus.innerText = "Player Two Wins!"
-
-        } else {
-            alert("player one wins!")
-            gameStatus.innerText = "Player One Wins!"
-
-        }
-        endGame()
-
+    if (diagonalCheck) {
+        alert("three in a diagonal")
     }
+
 }
 
-// Alternates between Player 1 and Player 2
-// Prevents players from placing a token in a taken spot
+
+
+
+
+
+
+
+// Able to alternate player and prevent player from clicking a taken spot
 function alternatePlayer(event) {
+    console.log(playerOneStatus)
     if (playerOneStatus) {
         if (event.target.innerText == "X" || event.target.innerText == "O") {
             alert("something here")
@@ -117,7 +131,7 @@ function alternatePlayer(event) {
             //x.setAttribute("id", "playerX")
             //x.innerHTML = "X"
             //event.target.appendChild(x)
-            gameStatus.innerText = "Player Two's Turn"
+            document.querySelector("h2").innerText = "Player Two's Turn"
             playerOneStatus = false
         }
     } else if (playerOneStatus === false) {
@@ -129,28 +143,8 @@ function alternatePlayer(event) {
             //o.setAttribute("id", "playerO")
             //o.innerHTML = "O"
             //event.target.appendChild(o)
-            gameStatus.innerText = "Player One's Turn"
+            document.querySelector("h2").innerText = "Player One's Turn"
             playerOneStatus = true
         }
     }
 }
-
-// Stops the game after a player has won or there is a draw
-function endGame() {
-    for (let i = 0; i < boxes.length; i++) {
-        boxes[i].removeEventListener("click", gamePlay)
-    }
-}
-
-// Allows button to reset the board and the game
-const resetButton = document.querySelector("#reset")
-resetButton.addEventListener("click", function (event) {
-    for (i = 0; i < boxes.length; i++) {
-        movesCounter = 0
-        boxes[i].innerText = ''
-        playerOneStatus = true
-        gameStatus.innerText = "Player One's Turn"
-        boxes[i].removeEventListener("click", gamePlay)
-        boxes[i].addEventListener("click", gamePlay)
-    }
-})
